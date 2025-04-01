c9d74f82d090098ba9df9870af5395ffc5f27f75
hansol04 <gksthf0428@naver.com>
2025-04-01 18:16:22 +0900

    250401

--- Ex08_MemberJpa01/src/main/java/com/study/springboot/controller/MemberController.java
+++ Ex08_MemberJpa01/src/main/java/com/study/springboot/controller/MemberController.java
@@ -27,17 +27,18 @@ public class MemberController {
 	@RequestMapping("/insert")
 	public String insert(@RequestParam("username") String name, Model model) {
 		/*
-		  Member member2 = new Member();
-		  member2.setUsername(name);
-		  member2.setCreateDate(LocalDate.now());
-		 */
+		Member member2 = new Member();
+		member2.setUsername(name);
+		member2.setCreateDate(LocalDate.now());
+		*/
 		Member member = Member.builder()
 							  .username(name)
 							  .createDate(LocalDate.now())
 							  .build();
+		
 		Member result = memberService.insert(member);
 		
-		model.addAttribute("member",result);
+		model.addAttribute("member", result);
 		return "insert";
 	}
 	
@@ -45,13 +46,13 @@ public class MemberController {
 	public String select(@RequestParam("id") Long id, Model model) {
 		/*
 		 * Optional<T> : NullpointerException 발생을 방지하기 위해
-		  				 기존의 반환 값 타입 T에 Optional<T>를 wrapping하여,
-		  				 null 대신 Optional 안에 빈 타입 객체를 돌려주는 기법	
-		   ex) Member member = memberRepository.findById("user01"); => 없는 아이디
-		   	   member.getUserName(); => NullpointerException
+		 				 기존의 반환 값 타입 T에 Optional<T>를 wrapping하여,
+		 				 null 대신 Optional안에 빈 타입 객체를 돌려주는 기법
+		    ex) Member member = memberRepository.findById("usr01");  = > 없는 아이디
+		    	member.getUserName(); => NullpointerException		
 		 */
 		Optional<Member> result = memberService.select(id);
-		if(result.isPresent()) {	// isPresent() : 데이터가 있는지 없는지 체크
+		if(result.isPresent()) {  // isPresent() : 데이터 있는지 없는지 체크
 			model.addAttribute("member", result.get());
 		} else {
 			model.addAttribute("member", null);
@@ -67,31 +68,35 @@ public class MemberController {
 	}
 	
 	@RequestMapping("/delete")
-	public String delete(@RequestParam("id") Long id, Model model) {
-		Optional<Member> result = memberService.delete(id);
-		if(result.isPresent()) {
-			model.addAttribute("member", result.get());
-		} else {
-			model.addAttribute("member", null);
-		}
-		return "delete";
+	public String delete(@RequestParam("id") Long id) {
+		memberService.delete(id);
+		return "menu";
 	}
 	
+	@RequestMapping("/update")
+	public String update(Member member, Model model) {
+		// update시 primary key를 검색하여 키가 있으면 다른 필드 모두 업데이트
+		// update시 기존내용을 업데이트 하지 않는다고 안넣으면 null이 들어감
+		// => select를 하여 그 결과에 update할 내용만 넣으면 됨
+		member.setCreateDate(LocalDate.now());
+		Member result = memberService.update(member);
+		model.addAttribute("member", result);
+		return "update";
+	}
+	
+	/*
+	@RequestMapping("/update")
+	public String update(@RequestParam("id") Long id,
+						 @RequestParam("username") String username,
+						 Model model) {
+		Member m = new Member();
+		m.setId(id);
+		m.setCreateDate(LocalDate.now());
+		m.setUsername(username);
+		Member result = memberService.update(m);
+		model.addAttribute("member", result);
+		return "update";
+	}
+	*/
 	
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
